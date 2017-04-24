cc625793f24ce3bb55cd443fe978a27c96febbf6
Alex Clemmer <clemmer.alexander@gmail.com>
2017-04-23 19:40:02 -0700

    [WIP] Move `onComplete` into `AnalysisEventListener` interface
    This commit will continue the work of breaking the language service down
    into interfaces that can be mocked, extended, and re-used in a variety
    of use cases (e.g., having a TypeScript frontend talk to a compiler over
    HTTP).
    
    This commit will specifically move the meaty chunk of code required for
    the `onComplete` event into the interface we use for evented analysis,
    called `AnalysisEventListener`.

--- server/ast/analyzer.ts
+++ server/ast/analyzer.ts
@@ -10,6 +10,10 @@ import * as ast from './node';
 import * as token from './token';
 import * as astVisitor from './visitor';
 
+//
+// Interfaces.
+//
+
 export interface CachedDocument {
   text: string | null
   parse: ast.Node | null
@@ -25,7 +29,10 @@ export interface WorkspaceEventListener {
 export interface AnalysisEventListener {
   // Allows us to mock vscode events, e.g., OnHover, OnComplete, etc.
 
-  onHover: (fileUri: string, cursorLoc: token.Location) => void
+  onHover: (fileUri: string, cursorLoc: token.Location) => Promise<HoverInfo>
+  onComplete: (
+    fileUri: string, docText: string, cursorLoc: token.Location
+  ) => Promise<CompletionInfo[]>
 }
 
 export interface DocumentManager {
@@ -47,6 +54,18 @@ export interface LanguageString {
 export interface HoverInfo {
   contents: LanguageString | LanguageString[]
 }
+
+export type CompletionType = "Field" | "Variable";
+
+export interface CompletionInfo {
+  label: string
+  kind: CompletionType
+  documentation?: string
+}
+
+//
+// Analyzer.
+//
 
 // TODO: Rename this to `EventedAnalyzer`.
 export class Analyzer implements WorkspaceEventListener,
@@ -119,15 +138,114 @@ AnalysisEventListener {
       });
   }
 
+  public onComplete = (
+    fileUri: string, docText: string, cursorLoc: token.Location
+  ): Promise<CompletionInfo[]> => {
+    const tokens = this.lexJsonnetText(docText, cursorLoc);
+
+    // The pass parameter contains the position of the text
+    // document in which code complete got requested. For the
+    // example we ignore this info and always provide the same
+    // completion items.
+    return new Promise<immutable.List<token.Token>>(
+      (resolve, reject) => {
+        try {
+          const tokens = this.lexJsonnetText(docText, cursorLoc);
+          resolve(findCompletionTokens(tokens, cursorLoc));
+        } catch (err) {
+          reject(err);
+        }
+      })
+      .then(tokens => {
+        const parseLastDocument = new Promise<ast.Node | null>(
+          (resolve, reject) => {
+            const lastSavedDoc = this.docCache.has(fileUri)
+              ? this.docCache.get(fileUri)
+              : null
+            resolve(lastSavedDoc && lastSavedDoc.parse || null);
+          });
+
+          return Promise.all([tokens, parseLastDocument]);
+      })
+      .then(([tokens, lastDocumentRoot]) => {
+        if (lastDocumentRoot == null) { return []; }
+
+        const nodeAtPos = this.getNodeAtPositionFromAst(
+          lastDocumentRoot, cursorLoc);
+
+        if (nodeAtPos == null || nodeAtPos.env == null) { return []; }
+        return this.completeTokens(tokens, nodeAtPos.env);
+      });
+  }
+
+  //
+  // Utilities.
+  //
+
+  private completeTokens = (
+    tokens: immutable.List<token.Token>,
+    env: ast.Environment,
+  ): CompletionInfo[] => {
+    const completion =
+      (label, kind, data): CompletionInfo => <CompletionInfo>{
+        label: label,
+        kind: kind,
+        data: data,
+      };
+
+    const tokenCount = tokens.count();
+    if (tokenCount == 0) {
+      return []
+    }
+    if (tokenCount == 1 || tokenCount == 2) {
+      // It's a `local` value. Autocomplete suggestions.
+      return envToSuggestions(env);
+    } else {
+      // Repeatedly consume tokens and attempt to populate autocomplete
+      // suggestions.
+      let tokenStream = tokens;
+      let currEnv = env;
+      let tokenCount = tokenStream.count();
+      let lastResolved: ast.Node | null = null;
+
+      while (true) {
+        const nextToken = tokens.first();
+        tokenStream = tokens.shift();
+        tokenCount--;
+
+        if (tokenCount == 1) {
+          // Last token. Autocomplete suggestions.
+          if (lastResolved == null) {
+            throw new Error("INTERNAL ERROR: lastResolved can't be null");
+          }
+          return getCompletableFields(lastResolved);
+        }
+
+        if (nextToken.kind == "TokenIdentifier") {
+          // Attempt to lookup identifier.
+          const node = this.resolveFromEnv(nextToken.data, currEnv);
+          if (node == null) { return []; }
+          if (node.env == null) {
+            throw new Error(`INTERNAL ERROR: A node environment should never be null ${ast.renderAsJson(node)}`);
+          }
+
+          if (lastResolved != null) {
+            return getCompletableFields(lastResolved);
+          }
+          lastResolved = node;
+          continue;
+        } else if (nextToken.kind == "TokenOperator") {
+          // Currently only `.` operator allowed.
+          if (nextToken.data !== ".") { return []; }
+          continue;
+        }
+      }
+    }
+  }
+
   //
   // The rest.
   //
-
-  public getFromCache(docUri: string): CachedDocument | null {
-    return this.docCache.has(docUri)
-      ? this.docCache.get(docUri)
-      : null;
-  }
 
   public resolveSymbolAtPosition = (
     filePath: string, pos: token.Location,
@@ -393,3 +511,100 @@ AnalysisEventListener {
     return rootNode;
   }
 }
+
+//
+// Utilities.
+//
+
+// findCompletionTokens finds all "completable" tokens starting from
+// the end of the token stream.
+const findCompletionTokens = (
+  tokens: immutable.List<token.Token>, loc: token.Location
+): immutable.List<token.Token> => {
+  let stop = false;
+  const completionTokens = tokens
+    .reverse()
+    // TODO: We might want to skip a terminal EOF here. It's not clear
+    // that autocomplete will work if the last token is an EOF.
+    .takeUntil(token => {
+      // TODO: This should be handled by the partial-parser, not us.
+
+      if (token == null || stop) {
+        return true;
+      }
+      switch (token.kind) {
+        case "TokenIdentifier": {
+          // Two subsequent identifier tokens are always separated by
+          // whitespace. We want only the last of these tokens.
+          if (token.fodder && token.fodder.length > 0) {
+            stop = true;
+          }
+          return false;
+        }
+        case "TokenDot": {
+          return false;
+        }
+        default: return true;
+      }
+    })
+    .reverse()
+    .toList();
+
+  // Append an EOF token if we don't have one, to avoid choking
+  // the parser.
+  const lastElement = completionTokens.last();
+  if (lastElement == null || lastElement.kind != "TokenEndOfFile") {
+    const eofToken: token.Token = {
+      kind: "TokenEndOfFile",
+      data: ".",
+      fodder: null,
+      loc: {
+        fileName: "",
+        begin: {line: -1, column: -1},
+        end: {line: -1, column: -1},
+      },
+    };
+    return completionTokens.push(eofToken);
+  } else {
+    return completionTokens;
+  }
+};
+
+const getCompletableFields = (node: ast.Node): CompletionInfo[] => {
+  if (node.nodeType != "ObjectNode") {
+    return []
+  }
+
+  const objNode = <ast.ObjectNode>node;
+  return objNode.fields.map(field => {
+    let id: string | null = null;
+    if (field.id != null) {
+      id = field.id.name;
+    } else {
+      console.log(`Only fields with ids are currently supported for autocomplete:\n${ast.renderAsJson(field)}`);
+    }
+    const docs = field.headingComments == null
+      ? null
+      : field.headingComments.map(comment => comment.text).join("\n\n");
+    return <CompletionInfo>{
+      label: id,
+      kind: "Field",
+      documentation: docs,
+    };
+  });
+}
+
+const envToSuggestions = (env: ast.Environment): CompletionInfo[] => {
+    return env.map((value, key) => {
+      if (value == null) {
+        throw new Error(`INTERNAL ERROR: Value in environment is null`);
+      }
+      return <CompletionInfo>{
+        label: key,
+        kind: "Variable",
+        // TODO: Fill in documentaiton later.
+      };
+    })
+    .toArray();
+}
+
--- server/server.ts
+++ server/server.ts
@@ -63,8 +63,13 @@ docs.listen(connection);
 connection.onInitialize((params) => initializer(docs, params));
 connection.onDidChangeConfiguration(params => configUpdateProvider(params));
 connection.onCompletion(position => {
-  const documentText = docs.get(position.textDocument.uri).getText();
-  return completionProvider(documentText, position);
+  return analyzer
+    .onComplete(
+      position.textDocument.uri,
+      docs.get(position.textDocument.uri).getText(),
+      positionToLocation(position))
+    .then<server.CompletionItem[]>(
+      completions => completions.map(completionInfoToCompletionItem));
 });
 connection.onHover(position => {
   const fileUri = position.textDocument.uri;
@@ -104,48 +109,6 @@ export const configUpdateProvider = (
   console.log(analyzer.command);
 }
 
-export const completionProvider = (
-  documentText: string,
-  position: server.TextDocumentPositionParams,
-): Promise<server.CompletionItem[]> => {
-
-  const docLocation = positionToLocation(position);
-  const tokens = analyzer.lexJsonnetText(documentText, docLocation);
-
-  // The pass parameter contains the position of the text
-  // document in which code complete got requested. For the
-  // example we ignore this info and always provide the same
-  // completion items.
-  return new Promise<immutable.List<token.Token>>(
-    (resolve, reject) => {
-      try {
-        const tokens = analyzer.lexJsonnetText(documentText, docLocation);
-        resolve(findCompletionTokens(tokens, docLocation));
-      } catch (err) {
-        reject(err);
-      }
-    })
-    .then(tokens => {
-      const parseLastDocument = new Promise<ast.Node | null>(
-        (resolve, reject) => {
-          const lastSavedDoc =
-            analyzer.getFromCache(position.textDocument.uri);
-          resolve(lastSavedDoc && lastSavedDoc.parse || null);
-        });
-
-        return Promise.all([tokens, parseLastDocument]);
-    })
-    .then(([tokens, lastDocumentRoot]) => {
-      if (lastDocumentRoot == null) { return []; }
-
-      const nodeAtPos = analyzer.getNodeAtPositionFromAst(
-        lastDocumentRoot, docLocation);
-
-      if (nodeAtPos == null || nodeAtPos.env == null) { return []; }
-      return completeTokens(tokens, nodeAtPos.env);
-    });
-};
-
 const positionToLocation = (
   posParams: server.TextDocumentPositionParams
 ): token.Location => {
@@ -155,155 +118,27 @@ const positionToLocation = (
   };
 }
 
-// findCompletionTokens finds all "completable" tokens starting from
-// the end of the token stream.
-const findCompletionTokens = (
-  tokens: immutable.List<token.Token>, loc: token.Location
-): immutable.List<token.Token> => {
-  let stop = false;
-  const completionTokens = tokens
-    .reverse()
-    // TODO: We might want to skip a terminal EOF here. It's not clear
-    // that autocomplete will work if the last token is an EOF.
-    .takeUntil(token => {
-      // TODO: This should be handled by the partial-parser, not us.
-
-      if (token == null || stop) {
-        return true;
+const completionInfoToCompletionItem = (
+  completionInfo: analyze.CompletionInfo
+): server.CompletionItem => {
+    let kindMapping: server.CompletionItemKind;
+    switch (completionInfo.kind) {
+      case "Field": {
+        kindMapping = server.CompletionItemKind.Field;
+        break;
       }
-      switch (token.kind) {
-        case "TokenIdentifier": {
-          // Two subsequent identifier tokens are always separated by
-          // whitespace. We want only the last of these tokens.
-          if (token.fodder && token.fodder.length > 0) {
-            stop = true;
-          }
-          return false;
-        }
-        case "TokenDot": {
-          return false;
-        }
-        default: return true;
+      case "Variable": {
+        kindMapping = server.CompletionItemKind.Field;
+        break;
       }
-    })
-    .reverse()
-    .toList();
-
-  // Append an EOF token if we don't have one, to avoid choking
-  // the parser.
-  const lastElement = completionTokens.last();
-  if (lastElement == null || lastElement.kind != "TokenEndOfFile") {
-    const eofToken: token.Token = {
-      kind: "TokenEndOfFile",
-      data: ".",
-      fodder: null,
-      loc: {
-        fileName: "",
-        begin: {line: -1, column: -1},
-        end: {line: -1, column: -1},
-      },
-    };
-    return completionTokens.push(eofToken);
-  } else {
-    return completionTokens;
-  }
-}
-
-const completeTokens = (
-  tokens: immutable.List<token.Token>,
-  env: ast.Environment,
-): server.CompletionItem[] => {
-  const completion =
-    (label, kind, data): server.CompletionItem => <server.CompletionItem>{
-      label: label,
-      kind: kind,
-      data: data,
-    };
-
-  const tokenCount = tokens.count();
-  if (tokenCount == 0) {
-    return []
-  }
-  if (tokenCount == 1 || tokenCount == 2) {
-    // It's a `local` value. Autocomplete suggestions.
-    return envToSuggestions(env).toArray();
-  } else {
-    // Repeatedly consume tokens and attempt to populate autocomplete
-    // suggestions.
-    let tokenStream = tokens;
-    let currEnv = env;
-    let tokenCount = tokenStream.count();
-    let lastResolved: ast.Node | null = null;
-
-    while (true) {
-      const nextToken = tokens.first();
-      tokenStream = tokens.shift();
-      tokenCount--;
-
-      if (tokenCount == 1) {
-        // Last token. Autocomplete suggestions.
-        if (lastResolved == null) {
-          throw new Error("INTERNAL ERROR: lastResolved can't be null");
-        }
-        return getCompletableFields(lastResolved);
-      }
-
-      if (nextToken.kind == "TokenIdentifier") {
-        // Attempt to lookup identifier.
-        const node = analyzer.resolveFromEnv(nextToken.data, currEnv);
-        if (node == null) { return []; }
-        if (node.env == null) {
-          throw new Error(`INTERNAL ERROR: A node environment should never be null ${ast.renderAsJson(node)}`);
-        }
-
-        if (lastResolved != null) {
-          return getCompletableFields(lastResolved);
-        }
-        lastResolved = node;
-        continue;
-      } else if (nextToken.kind == "TokenOperator") {
-        // Currently only `.` operator allowed.
-        if (nextToken.data !== ".") { return []; }
-        continue;
-      }
+      default: throw new Error(
+        `Unrecognized completion type '${completionInfo.kind}'`);
     }
-  }
+
+    // Black magic type coercion. This allows us to avoid doing a
+    // deep copy over to a new `CompletionItem` object, and
+    // instead only re-assign the `kindMapping`.
+    const completionItem = (<server.CompletionItem>(<object>completionInfo));
+    completionItem.kind = kindMapping;
+    return completionItem;
 }
-
-const getCompletableFields = (node: ast.Node): server.CompletionItem[] => {
-  if (node.nodeType != "ObjectNode") {
-    return []
-  }
-
-  const objNode = <ast.ObjectNode>node;
-  return objNode.fields.map(field => {
-    let id: string | null = null;
-    if (field.id != null) {
-      id = field.id.name;
-    } else {
-      console.log(`Only fields with ids are currently supported for autocomplete:\n${ast.renderAsJson(field)}`);
-    }
-    const docs = field.headingComments == null
-      ? null
-      : field.headingComments.map(comment => comment.text).join("\n\n");
-    return <server.CompletionItem>{
-      label: id,
-      kind: server.CompletionItemKind.Field,
-      documentation: docs,
-    };
-  });
-}
-
-const envToSuggestions = (env: ast.Environment) => {
-    return env.map((value, key) => {
-      if (value == null) {
-        throw new Error(`INTERNAL ERROR: Value in environment is null`);
-      }
-      return <server.CompletionItem>{
-        label: key,
-        kind: server.CompletionItemKind.Variable,
-        // TODO: Fill in documentaiton later.
-      };
-    });
-}
-
