b9cd4d6ab79534b9407f136b32c3f4b7c9140603
Alex Clemmer <clemmer.alexander@gmail.com>
2017-05-11 18:34:10 -0700

    Display diagnostic information when user types an error
    vscode currently allows language extensions to highlight diagnostic
    information with squiggly underlines. This is useful for, e.g.,
    highlighting parse errors.
    
    This commit will hook these facilities up to the Jsonnet parser we've
    written, so that syntactic analysis errors are highlighted to the user.

--- server/server.ts
+++ server/server.ts
@@ -10,6 +10,7 @@ import * as ast from './parser/node';
 import * as error from './lexer/static_error';
 import * as lexer from './lexer/lexer';
 import * as local from './local';
+import * as compile from "./ast/compiler";
 import * as service from './ast/service';
 
 // Create a connection for the server. The connection uses Node's IPC
@@ -28,6 +29,50 @@ const analyzer: analyze.EventedAnalyzer = new analyze.Analyzer(
   new local.VsDocumentManager(docs),
   compiler);
 
+const failureToDiagnostic = (
+  error: compile.LexFailure | compile.ParseFailure
+): server.Diagnostic => {
+  let begin: error.Location | null = null;
+  let end: error.Location | null = null;
+  let message: string | null = null;
+  if (compile.isLexFailure(error)) {
+    begin = error.lexError.loc.begin;
+    end = error.lexError.loc.end;
+    message = error.lexError.msg;
+  } else {
+    begin = error.parseError.loc.begin;
+    end = error.parseError.loc.end;
+    message = error.parseError.msg;
+  }
+
+  return {
+    severity: server.DiagnosticSeverity.Error,
+    range: {
+      start: {line: begin.line - 1, character: begin.column - 1},
+      end: {line: end.line - 1, character: end.column - 1},
+    },
+    message: `${message}`,
+    source: `Jsonnet`,
+  };
+}
+
+const generateDiagnostics = (doc: server.TextDocument) => {
+  const text = doc.getText();
+  const results = compiler.cache(doc.uri, text, doc.version);
+
+  if (compile.isParsedDocument(results)) {
+    connection.sendDiagnostics({
+      uri: doc.uri,
+      diagnostics: [],
+    });
+  } else {
+    connection.sendDiagnostics({
+      uri: doc.uri,
+      diagnostics: [failureToDiagnostic(results.parse)],
+    });
+  }
+};
+
 //
 // TODO: We should find a way to move these hooks to a "init doc
 // manager" method, or something.
@@ -39,6 +84,7 @@ const analyzer: analyze.EventedAnalyzer = new analyze.Analyzer(
 docs.onDidOpen(openEvent => {
   const doc = openEvent.document;
   if (doc.languageId === "jsonnet") {
+    generateDiagnostics(doc);
     return analyzer.onDocumentOpen(doc.uri, doc.getText(), doc.version);
   }
 });
@@ -49,6 +95,7 @@ docs.onDidSave(saveEvent => {
   // that are single-line, and those that are multi-line.
   const doc = saveEvent.document;
   if (doc.languageId === "jsonnet") {
+    generateDiagnostics(doc);
     return analyzer.onDocumentOpen(doc.uri, doc.getText(), doc.version);
   }
 });
@@ -58,6 +105,11 @@ docs.onDidClose(closeEvent => {
   // dependencies we save.
   if (closeEvent.document.languageId === "jsonnet") {
     return analyzer.onDocumentClose(closeEvent.document.uri);
+  }
+});
+docs.onDidChangeContent(changeEvent => {
+  if (changeEvent.document.languageId === "jsonnet") {
+    generateDiagnostics(changeEvent.document);
   }
 });
 
